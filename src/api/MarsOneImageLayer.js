79e454ae1cb47dcdf60469de8ebbe3575db4a1e6
Bruce Schubert <bruce@emxsys.com>
2018-08-29 04:58:06 -0700

    Added MarsOneImageLayer base layer. - Uses image from http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/MDIM21/thumbs/Mars_Viking_MDIM21_ClrMosaic_global_1024.jpg

---
+++ src/api/MarsOneImageLayer.js
@@ -0,0 +1,25 @@
+/* 
+ * Copyright (c) 2018 Bruce Schubert.
+ * The MIT License
+ * http://www.opensource.org/licenses/mit-license
+ */
+import 'worldwindjs'; //WorldWind global
+
+/* global WorldWind */
+
+/**
+ * A Mars image layer that spans the entire globe.
+ */
+export default class MarsOneImageLayer extends WorldWind.RenderableLayer {
+  constructor(title) {
+    super(title || "Mars");
+    
+    let surfaceImage = new WorldWind.SurfaceImage(WorldWind.Sector.FULL_SPHERE,
+      WorldWind.configuration.baseUrl + "images/Mars_Viking_MDIM21_ClrMosaic_global_1024.jpg");
+    this.addRenderable(surfaceImage);
+
+    this.pickEnabled = false;
+
+    this.minActiveAltitude = 3e6;
+  }
+}
